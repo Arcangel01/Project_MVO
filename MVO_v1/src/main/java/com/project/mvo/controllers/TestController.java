61d31316a49d2d882c3efd8c571feaf6f8c9f3d3
Arcangel Catanio <arcangel_021@outlook.com>
2020-09-20 17:55:11 -0500

    Cargando el WebConfig para el manejo de los cors

--- MVO_v1/src/main/java/com/project/mvo/controllers/TestController.java
+++ MVO_v1/src/main/java/com/project/mvo/controllers/TestController.java
@@ -3,7 +3,6 @@ package com.project.mvo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.project.mvo.models.Test;
 import com.project.mvo.services.TestService;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping({ "/v1" })
 public class TestController {
